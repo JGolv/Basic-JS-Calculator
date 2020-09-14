c8e91fed8767b188e82ab5b881fc485eb68449bc
JGolv <juliangolv@gmail.com>
2020-09-14 12:42:57 +0200

    Added javascript functionality

---
+++ calculator.js
@@ -0,0 +1,127 @@
+class Calculator {
+    constructor(prevOperandTextElem, currOperandTextElem) {
+        this.prevOperandTextElem = prevOperandTextElem
+        this.currOperandTextElem = currOperandTextElem
+        this.clear()
+    }
+
+    clear() {
+        this.currOperand = ''
+        this.prevOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+        this.currOperand = this.currOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(num) {
+        if(this.currOperand.includes('.') && num === '.') return
+        this.currOperand = this.currOperand.toString() + num.toString()
+    }
+
+    chooseOperation(operation) {
+        if(this.currOperand === '') return
+        if(this.prevOperand !== '') {
+            this.processValue()
+        }
+        this.operation = operation
+        this.prevOperand = this.currOperand
+        this.currOperand = ''
+    }
+
+    processValue() {
+        let processing
+        const prev = parseFloat(this.prevOperand)
+        const curr = parseFloat(this.currOperand)
+        if(isNaN(prev) || isNaN(curr)) return
+
+        switch (this.operation) {
+            case '+':
+              processing = prev + curr
+              break
+            case '-':
+              processing = prev - curr
+              break
+            case '*':
+              processing = prev * curr
+              break
+            case '÷':
+              processing = prev / curr
+              break
+            default:
+              return
+          }
+          this.currOperand = processing
+          this.operation = undefined
+          this.prevOperand = ''
+    }
+
+    getDisplayNumber(num) {
+        const stringNumber = num.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+      }
+
+    updateScreen() {
+        this.currOperandTextElem.innerText = this.getDisplayNumber(this.currOperand)
+        if(this.operation != null) {
+            this.prevOperandTextElem.innerText = 
+            `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`
+        } else {
+            this.prevOperandTextElem.innerText = ''
+        }
+    }
+}
+
+//Created variables for data attributes so we can get the data from an item
+const numberBtns = document.querySelectorAll('[data-num]')
+const operationBtns = document.querySelectorAll('[data-fn]')
+const equalsBtn = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-del]')
+const allClearButton = document.querySelector('[data-ac]')
+const prevOperandTextElem = document.querySelector('[data-prev]')
+const currOperandTextElem = document.querySelector('[data-curr]')
+
+const calculator = new Calculator(prevOperandTextElem,currOperandTextElem)
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateScreen();
+    })
+})
+
+operationBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateScreen();
+    })
+})
+
+
+equalsBtn.addEventListener('click', button => {
+    calculator.processValue()
+    calculator.updateScreen();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateScreen();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateScreen();
+})

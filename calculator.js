daf27257d04b62232d86b8453db99963732fb33d
JGolv <juliangolv@gmail.com>
2020-09-14 13:08:14 +0200

    Added comments explaining briefly how most of it works

--- calculator.js
+++ calculator.js
@@ -5,21 +5,25 @@ class Calculator {
         this.clear()
     }
 
+    //Clears the calculator fields and the operator of the last calculation
     clear() {
         this.currOperand = ''
         this.prevOperand = ''
         this.operation = undefined
     }
 
+    //Deletes last character input
     delete() {
         this.currOperand = this.currOperand.toString().slice(0, -1);
     }
 
+    //Concatenation of the current operand
     appendNumber(num) {
         if(this.currOperand.includes('.') && num === '.') return
         this.currOperand = this.currOperand.toString() + num.toString()
     }
 
+    //Processes the operation being made and puts the first value in the previous value field
     chooseOperation(operation) {
         if(this.currOperand === '') return
         if(this.prevOperand !== '') {
@@ -30,6 +34,7 @@ class Calculator {
         this.currOperand = ''
     }
 
+    //Picks the corresponding operation and puts the result in the previous value field
     processValue() {
         let processing
         const prev = parseFloat(this.prevOperand)
@@ -57,6 +62,8 @@ class Calculator {
           this.prevOperand = ''
     }
 
+    /* This method parses the values before and after the decimal point according to a certain
+    locale string (in our case it is ES) */
     getDisplayNumber(num) {
         const stringNumber = num.toString()
         const integerDigits = parseFloat(stringNumber.split('.')[0])
@@ -65,7 +72,7 @@ class Calculator {
         if (isNaN(integerDigits)) {
           integerDisplay = ''
         } else {
-          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+          integerDisplay = integerDigits.toLocaleString('ES', { maximumFractionDigits: 0 })
         }
         if (decimalDigits != null) {
           return `${integerDisplay}.${decimalDigits}`
@@ -74,6 +81,7 @@ class Calculator {
         }
       }
 
+    //Updates both fields in the screen depending on the values and the operations
     updateScreen() {
         this.currOperandTextElem.innerText = this.getDisplayNumber(this.currOperand)
         if(this.operation != null) {
@@ -96,6 +104,10 @@ const currOperandTextElem = document.querySelector('[data-curr]')
 
 const calculator = new Calculator(prevOperandTextElem,currOperandTextElem)
 
+/* These foreach loops allow us to bind the different data attributes in the HTML with the 
+variables in the script, making it easier to process the different events */
+
+//This binds the html numbers with its value
 numberBtns.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
@@ -103,6 +115,7 @@ numberBtns.forEach(button => {
     })
 })
 
+//This binds the html operation buttons with specific operations such as adding or substracting
 operationBtns.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
@@ -110,17 +123,20 @@ operationBtns.forEach(button => {
     })
 })
 
-
+//This binds the equal button with the script to process the result and show it
 equalsBtn.addEventListener('click', button => {
     calculator.processValue()
     calculator.updateScreen();
 })
 
+
+//This binds the AC button to clear the memory of the calculator
 allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateScreen();
 })
 
+//This binds the DEL button to delete the last character of the current number
 deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateScreen();
